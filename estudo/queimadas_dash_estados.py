e28f86be3de1ff217b2241e6820c7c7157a1f231
Eduardo Mariot <eduardo.mariot@gmail.com>
2021-03-30 03:39:11 -0300

    Add files via upload

--- estudo/queimadas_dash_estados.py
+++ estudo/queimadas_dash_estados.py
@@ -19,6 +19,7 @@ import dash_table
 from dash_table.Format import Format, Group, Scheme, Symbol
 
 df = pd.read_csv('dataset/historico_bioma_estados.csv', encoding='latin-1')
+df_texto_ano = pd.read_csv('dataset/info-anos.csv',  encoding='latin-1', sep=';')
 
 year_options = []
 for ano in df['Ano'].unique():
@@ -51,7 +52,63 @@ app.layout = html.Div([
                                'paddingTop':'40px',
                                'paddingBottom':'40px'}
                 ), 
-            boot.Row(), # Texto + Popover
+            boot.Row( # Texto + Popover
+                     [
+                     
+                    boot.Col( # Texto
+                        html.Label("Escolha um ano:"),
+                        width = 3,
+                        align = 'left',
+                        style={'display':'inline-block'}
+                        ),
+                    boot.Col( # Popover
+                        html.Div(
+                            [
+                                boot.Button(
+                                    "+ info",
+                                    outline = True,
+                                    id = 'popovertarget-mapa',
+                                    style = {
+                                        'fontFamily': 'Garamond'
+                                        },
+                                    className='mr-2',
+                                    color='success',
+                                    size='sm'
+                                    ),
+                                boot.Popover(
+                                    [
+                                    boot.PopoverHeader(id='popover-header-mapa'),
+                                    boot.PopoverBody(
+                                        dcc.Markdown(
+                                            id='popover-body-mapa',
+                                            style={
+                                                'textAlign':'justify'
+                                                }
+                                            ),style={
+                                                'overflow': 'auto',
+                                                'max-height': '500px'
+                                                }
+                                    
+                                        )
+                                    
+                                    ],
+                                    id ='popover-mapa',
+                                    target='popovertarget-mapa',
+                                    placement='bottom-end',
+                                    is_open = False
+                                    
+                                    ),
+                                
+                                ]
+                            ),
+                            width = 2,
+                            align = 'right'
+                                    
+                        )
+                         ], style = {'paddingLeft': '12%', 'paddingRight': '5%'},
+                            justify='between'
+                     
+                     ),
             boot.Row( # Dropdown
                 boot.Col(
                     dcc.Dropdown(
@@ -74,7 +131,28 @@ app.layout = html.Div([
     html.Div(), # Dados separados por Estados
     html.Div(), # Footer
     ])
+
+# Header para o popover do mapa
+@app.callback(Output('popover-header-mapa', 'children'),
+              [Input('year-picker', 'value')])
+def update_pop_over_header_mapa(selected_year):
+    return "Brasil em "+str(selected_year)
+                                
+# Conteúdo do corpo para o popover do mapa                                
+@app.callback(Output('popover-body-mapa', 'children'),
+              [Input('year-picker', 'value')])
+def update_pop_over_body_mapa(selected_year):
+    return df_texto_ano[df_texto_ano['Ano'] == selected_year]['Texto']                   
             
+# Alterando o estado do popover, de False para True, de True para False ao clicar
+@app.callback(Output("popover-mapa", "is_open"),
+              [Input('popovertarget-mapa', "n_clicks")],
+              [State("popover-mapa", "is_open")])
+def toggle_popover_mapa(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
 @app.callback(Output('title-year', 'children'),
               [Input('year-picker', 'value')])
 def update_mape(selected_year):
